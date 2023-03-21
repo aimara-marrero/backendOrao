7102c08c1a1358bb2fc88cde8fe48c9fadad124a
aimara-marrero <aimara.bmarrero@gmail.com>
2023-03-21 10:56:48 +0000

    create cookie and sent it with the res

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,29 +12,45 @@ const getUsers = async (req, res, next) => {
         };
         
         const registerUser = async (req, res, next) => {
-          try {
-            const { name, lastName, email, password } = req.body;
-            if (!(name && lastName && email && password)) {
-              return res.status(400).send("All inputs are required");
+            try {
+              const { name, lastName, email, password } = req.body;
+              if (!(name && lastName && email && password)) {
+                return res.status(400).send("All inputs are required");
+              }
+          
+              const userExists = await User.findOne({ email });
+              if (userExists) {
+                return res.status(400).send("user exists");
+              } else {
+                const hashedPassword = hashPassword(password);
+                const user = await User.create({
+                  name,
+                  lastName,
+                  email: email.toLowerCase(),
+                  password: hashedPassword,
+                });
+                res
+                .cookie("access_token", "fake access token", {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: "strict"
+                })
+                  .status(201)
+                  .json({
+                    success: "User created",
+                    userCreated: {
+                      _id: user._id,
+                      name: user.name,
+                      lastName: user.lastName,
+                      email: user.email,
+                      isAdmin: user.isAdmin,
+                    },
+                  });
+              }
+            } catch (err) {
+              next(err);
             }
-        
-            const userExists = await User.findOne({ email });
-            if (userExists) {
-              return res.status(400).json({ error: "user exists" });
-            } else {
-              const hashedPassword =  hashPassword(password)
-              const user = await User.create({
-                name,
-                lastName,
-                email: email.toLowerCase(),
-                password: hashedPassword
-              });
-              res.status(201).send(user)
-            }
-          } catch (err) {
-            next(err);
-          }
-        };
+          };
         
         module.exports = {getUsers, registerUser};
         
