936a90b2a07304f3c6c25008e62f467f21e4c3e0
Aimara <aimara.bmarrero@gmail.com>
2023-03-22 08:23:51 +0000

    Establecidas categorias - pendiente IMG

--- seeder/categories.js
+++ seeder/categories.js
@@ -1,17 +1,23 @@
 const categories = [
-    {
-    name: "Woman",
-    description:
-      "Woman - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
-    image: "/images/tablets-category.png",
-    
-  },
   {
-    name: "Man",
-    description:
-      "Man - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
-    image: "/images/tablets-category.png",
-  },
+  name: "Woman",
+  description:
+    "Woman - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+  image: "/images/tablets-category.png",
+
+},
+{
+  name: "Man",
+  description:
+    "Man - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+  image: "/images/tablets-category.png",
+},
+{
+  name: "Kids",
+  description:
+    "Kids - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+  image: "/images/tablets-category.png",
+}
 ]
 
 module.exports = categories
