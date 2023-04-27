e5347460c75bfcd31e842a0bb046a4d14e2cc981
aimara-marrero <aimara.bmarrero@gmail.com>
2023-04-27 15:16:37 +0100

    Try to Chat Message

--- server.js
+++ server.js
@@ -9,10 +9,23 @@ const app = express();
 const httpServer = createServer(app);
 global.io = new Server(httpServer);
 
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
+
+io.on("connection", (socket) => {
+
+  //msg es el mensaje que es enviado desde userChatComponent'message from client'
+    socket.on("client sends message", (msg) => {
+      socket.broadcast.emit("server sends message from client to admin", {
+        message: msg, 
+    })
+  })
+  //msg es el mensaje que es enviado desde adminChatComponent'message from admin'
+  socket.on("admin sends message", ({ message }) => {
+    socket.broadcast.emit("server sends message from admin to client", message);
+})
+})
 
 const apiRoutes = require("./routes/apiRoutes");
 
