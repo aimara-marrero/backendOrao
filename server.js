68fc3901fd3ec31b918a4f8b22c82e9b936bab48
Aimara <aimara.bmarrero@gmail.com>
2023-05-08 14:51:54 +0100

    change port to 4000

--- server.js
+++ server.js
@@ -103,6 +103,6 @@ app.use((error, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
